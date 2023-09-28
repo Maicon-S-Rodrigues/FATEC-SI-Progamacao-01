0174c2db9df38316d6f7266ca4a094e40a2974c0
Maicon Siqueira Rodrigues <110917695+Maicon-S-Rodrigues@users.noreply.github.com>
2023-09-28 20:23:22 -0300

    Add files via upload

---
+++ Programacao-01/Phyton/Basico_Inicio/TEMPERATURAFAHRENHEITPARACELCIUS.py
@@ -0,0 +1,5 @@
+fahrenheit = float(input('Informe a Temperatura Fahrenheit que deseja converter: °F '))
+
+celcius = (5 / 9) * (fahrenheit - 32)
+
+print('A temperatura em Celsius é: %3.f' % celcius, '°C')
---
+++ Programacao-01/Phyton/IF_Else_Segunda_Aula_Python/VerificarMaiorNumero.py
@@ -0,0 +1,9 @@
+A = float(input('Informe o Valor A: '))
+B = float(input('Informe o Valor B: '))
+
+if(A > B) :
+    print('O maior valor é A.')
+elif(B > A) :
+    print('O maior valor é B.')
+else :
+    print('Os valores A e B são iguais!')
