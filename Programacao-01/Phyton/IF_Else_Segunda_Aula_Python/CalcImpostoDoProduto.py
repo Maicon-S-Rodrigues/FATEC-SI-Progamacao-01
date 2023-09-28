e645d043f58d5e532957c024b0a870cf374c7a0f
Maicon Siqueira Rodrigues <110917695+Maicon-S-Rodrigues@users.noreply.github.com>
2023-09-28 20:48:31 -0300

    Add files via upload

---
+++ Programacao-01/Phyton/IF_Else_Segunda_Aula_Python/CalcImpostoDoProduto.py
@@ -0,0 +1,12 @@
+valor = float(input('Informe o Valor do produto: '))
+importado = input('Este Produto é importado?\n( S )-> Sim  || ( N )-> Não)\n')
+print(importado)
+if(importado.upper() == "S") :
+    valor = valor + (valor * 10) / 100
+    print("Valor do produto Nacional: " , valor)
+
+elif(importado.upper() == "N") :
+    valor = valor + (valor * 5) / 100
+    print("Valor do produto Nacional: " , valor)
+else :
+    print("Opção Invalida!")
