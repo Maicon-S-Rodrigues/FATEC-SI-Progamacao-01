df81b81741d6292c0ed0964d7bf7579545e9a59d
Maicon Siqueira Rodrigues <110917695+Maicon-S-Rodrigues@users.noreply.github.com>
2023-09-28 20:02:51 -0300

    Add files via upload

---
+++ Programacao-01/Phyton/Exercicios_Inicio/Basico_Inicio/CALCULARCONTADEAGUA.py
@@ -0,0 +1,5 @@
+litrosDeAgua = float(input('Informe quantos litros de água foram consumidos: '))
+
+valorDaConta = litrosDeAgua * 0.05
+
+print('O valor da conta de água é: R$ %.2f' % valorDaConta)
---
+++ Programacao-01/Phyton/Exercicios_Inicio/Basico_Inicio/VELOCIDADEMEDIA.py
@@ -0,0 +1,6 @@
+distancia = float(input('Informe a distancia percorrida: '))
+tempoGasto = float(input('Informe o tempo que a viagem durou: '))
+
+velocidadeMedia = distancia / tempoGasto
+
+print('A velocidade média foi: %.2f' % velocidadeMedia)
