7e0bec02b8b953a95a6bc0497272f102639cbc0a
Maicon Siqueira Rodrigues <110917695+Maicon-S-Rodrigues@users.noreply.github.com>
2023-09-28 20:02:28 -0300

    Delete Programacao-01/Phyton directory

--- Programacao-01/Phyton/Exercícios-Inicio/CALCSALARIOECOMISSAO.py
+++
@@ -1,8 +0,0 @@
-salarioFixo = float(input("Informe o Salario do Funcionario: R$ "))
-valorDeVendas = float(input("Informe o valor total das vendas realizadas por este Funcionario: R$ "))
-
-comissao = (valorDeVendas * 4) / 100
-salarioFinal = salarioFixo + comissao
-
-print("Valor de Comissão: R$ " , comissao)
-print("Valor do Salario Final: R$ " , salarioFinal)
--- Programacao-01/Phyton/Exercícios-Inicio/TEMPERATURAFAHRENHEITPARACELCIUS.py
+++
@@ -1,5 +0,0 @@
-fahrenheit = float(input('Informe a Temperatura Fahrenheit que deseja converter: °F '))
-
-celcius = (5 / 9) * (fahrenheit - 32)
-
-print('A temperatura em Celsius é: %3.f' % celcius, '°C')
