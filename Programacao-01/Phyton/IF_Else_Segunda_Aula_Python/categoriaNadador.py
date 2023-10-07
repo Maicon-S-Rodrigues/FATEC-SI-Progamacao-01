cb4d8321fa12abb0fdc816fb50f2295d591de56c
Maicon Siqueira Rodrigues <110917695+Maicon-S-Rodrigues@users.noreply.github.com>
2023-10-07 12:50:12 -0300

    Create categoriaNadador.py

---
+++ Programacao-01/Phyton/IF_Else_Segunda_Aula_Python/categoriaNadador.py
@@ -0,0 +1,10 @@
+idade = int(input("Informe a idade do nadador: "))
+
+if idade < 9:
+    print("Mirim")
+elif idade < 14:
+    print("Infantil")
+elif idade < 18:
+    print("Juvenil")
+else
+    print("Adulto")
